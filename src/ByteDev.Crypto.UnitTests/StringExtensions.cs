30b2ec0c4d2dc2896fca5b0e6880f9aad3d450ca
Paul Stanley <paulstanley@protonmail.com>
2018-09-22 10:50:30 +0100

    Added Sha1Algorithm

--- src/ByteDev.Crypto.UnitTests/StringExtensions.cs
+++ src/ByteDev.Crypto.UnitTests/StringExtensions.cs
@@ -1,10 +1,12 @@
-﻿namespace ByteDev.Crypto.UnitTests
+﻿using System.Text;
+
+namespace ByteDev.Crypto.UnitTests
 {
     internal static class StringExtensions
     {
         public static byte[] GetBytes(this string text)
         {
-            return System.Text.Encoding.Default.GetBytes(text);
+            return Encoding.Default.GetBytes(text);
         }
     }
 }
