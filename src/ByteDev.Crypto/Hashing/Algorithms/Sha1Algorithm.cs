9438880d6ec6a38d4e9c92146171596def81261b
Paul Stanley <paulstanley@protonmail.com>
2018-09-22 10:51:02 +0100

    Added file

---
+++ src/ByteDev.Crypto/Hashing/Algorithms/Sha1Algorithm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ByteDev.Crypto.Hashing.Algorithms
+{
+    public class Sha1Algorithm : IHashAlgorithm
+    {
+        public byte[] Hash(byte[] data)
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data));
+            if (data.Length < 1)
+                return new byte[0];
+
+            using (var sha1 = new SHA1Managed())
+            {
+                return sha1.ComputeHash(data);
+            }
+        }
+    }
+}
