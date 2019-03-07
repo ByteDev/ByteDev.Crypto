38617d597b5632a06bc01a57c975a7cc20464e53
Paul Stanley <paul.stanley@asos.com>
2019-03-07 15:15:10 +0000

    Added CharacterPositionChecker

---
+++ src/ByteDev.Crypto.UnitTests/CharacterPositionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ByteDev.Crypto.UnitTests
+{
+    [TestFixture]
+    public class CharacterPositionTests
+    {
+        [Test]
+        public void WhenPositionLessThanZero_ThenThrowException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new CharacterPosition('A', -1));
+        }
+
+        [Test]
+        public void WhenParamsValid_ThenSetProperties()
+        {
+            var sut = new CharacterPosition('A', 0);
+
+            Assert.That(sut.Character, Is.EqualTo('A'));
+            Assert.That(sut.Position, Is.EqualTo(0));
+        }
+    }
+}
