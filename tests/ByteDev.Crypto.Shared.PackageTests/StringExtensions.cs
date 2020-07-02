896d562898320f2b6546a5af3da2d891235679af
Paul Stanley <paulstanley@protonmail.com>
2020-07-02 11:55:00 +0800

    Update package tests

--- tests/ByteDev.Crypto.Shared.PackageTests/StringExtensions.cs
+++ tests/ByteDev.Crypto.Shared.PackageTests/StringExtensions.cs
@@ -1,23 +1,10 @@
-﻿using System.Text;
-
-namespace ByteDev.Crypto.Shared.PackageTests
+﻿namespace ByteDev.Crypto.Shared.PackageTests
 {
     internal static class StringExtensions
     {
         public static byte[] GetBytes(this string source)
         {
-            return Encoding.Default.GetBytes(source);
-        }
-
-        public static bool IsDigitsOnly(this string source)
-        {
-            foreach (char c in source)
-            {
-                if (c < '0' || c > '9')
-                    return false;
-            }
-
-            return true;
+            return System.Text.Encoding.Default.GetBytes(source);
         }
     }
 }
